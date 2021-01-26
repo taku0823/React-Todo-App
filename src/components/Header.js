84a8f616a0ab2d67338ba8e9ecbf5874f9e7157f
Taku Inoue <>
2021-01-26 23:58:06 +0900

    Create TodoApp and Header Components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/Header.css";
+
+function Header() {
+  return (
+    <header className="Header">
+      <h1>React Todo App</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/TodoApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/TodoApp.css";
+
+function TodoApp() {
+  return (
+    <div className="TodoApp">
+      <h1>React Todo App!</h1>
+    </div>
+  );
+}
+
+export default TodoApp;
