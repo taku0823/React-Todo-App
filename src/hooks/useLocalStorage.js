824cf94d830e5d2a4a7b2396aa64ad3ef1b68273
Taku Inoue <>
2021-03-08 22:47:39 +0900

    Update useLocalSorage.js

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,7 +3,14 @@ import { useState, useEffect } from "react";
 function useLocalStorage(key, initState) {
   const [state, setState] = useState(() => {
     const val = JSON.parse(window.localStorage.getItem(key));
-    return val.length ? val : initState;
+    let initVal;
+    try {
+      initVal = val.length ? val : initState;
+    } catch (err) {
+      console.warn(err);
+      initVal = initState;
+    }
+    return initVal;
   });
   useEffect(() => {
     window.localStorage.setItem(key, JSON.stringify(state));
